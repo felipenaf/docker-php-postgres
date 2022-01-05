22b4457d06491e4187f63a73688823fa60654267
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2022-01-05 11:42:24 -0300

    db test

--- php/info.php
+++ php/info.php
@@ -4,7 +4,7 @@ $consulta = sprintf(
     "host=%s port=%s dbname=%s user=%s password=%s",
     "postgres",
     5432,
-    "VAM",
+    "test",
     "postgres",
     "postgres"
 );
@@ -14,10 +14,9 @@ $conn = pg_connect($consulta);
 if ($conn) {
     pg_set_client_encoding($conn, 'UTF8');
 
-    $stmt = pg_query_params($conn, 'select * from city', []);
-    $result = pg_fetch_all($stmt);
-    
-    var_dump($result);
+    print "<h1>Connection Succeed</h1>";
+} else {
+    print "<h1>Connection Fail</h1>";
 }
 
 // phpinfo();
