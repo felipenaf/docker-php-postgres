4ed2b68d15f5b5ac8992f5237ea80e10ab88434a
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2022-01-05 11:50:40 -0300

    index

--- php/info.php
+++ php/info.php
@@ -1,22 +1,3 @@
 <?php
 
-$consulta = sprintf(
-    "host=%s port=%s dbname=%s user=%s password=%s",
-    "postgres",
-    5432,
-    "test",
-    "postgres",
-    "postgres"
-);
-
-$conn = pg_connect($consulta);
-
-if ($conn) {
-    pg_set_client_encoding($conn, 'UTF8');
-
-    print "<h1>Connection Succeed</h1>";
-} else {
-    print "<h1>Connection Fail</h1>";
-}
-
-// phpinfo();
+phpinfo();
